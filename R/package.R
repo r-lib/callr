8d1e095150a1bebf40d98acbe24e88737d5f959d
Gabor Csardi <csardi.gabor@gmail.com>
2016-05-13 11:25:35 +0100

    Fix up a Mason bug

--- R/package.R
+++ R/package.R
@@ -1,9 +1,9 @@
 
 #' Call R from R
 #'
-#' It is sometimes useful to perform a computation in a separate R process,
-  without affecting the current R process at all. This packages does exactly
-  that.
+#' It is sometimes useful to perform a computation in a separate R
+#' process, without affecting the current R process at all. This packages
+#' does exactly that.
 #'
 #' @docType package
 #' @name callr
