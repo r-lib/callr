efac258ded5f0076bad026c3351392d802e08cf2
Gábor Csárdi <csardi.gabor@gmail.com>
2016-05-15 11:19:17 +0100

    Make script file a single line
    The output is cleaner a bit, if --slave will be
    overridden by the user.

--- R/script.R
+++ R/script.R
@@ -1,9 +1,7 @@
 
 make_vanilla_script <- function(expr_file, res) {
 
-  script <- "
-    saveRDS(do.call(do.call, readRDS(\"{expr_file}\")), file = \"{res}\")
-  "
+  script <- "saveRDS(do.call(do.call, readRDS(\"{expr_file}\")), file = \"{res}\")"
 
   tmp <- tempfile()
   script <- gsub("{expr_file}", expr_file, script, fixed = TRUE)
