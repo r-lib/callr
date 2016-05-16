a822e1d0216b7be71196766ad600c78e374c9c40
Gábor Csárdi <csardi.gabor@gmail.com>
2016-05-16 10:24:37 +0100

    Refine the script, evaluate everything from .GlobalEnv

--- R/script.R
+++ R/script.R
@@ -1,7 +1,30 @@
 
 make_vanilla_script <- function(expr_file, res) {
 
-  script <- "saveRDS(do.call(do.call, readRDS(\"{expr_file}\")), file = \"{res}\")"
+  ## The function to run and its arguments are saved as list:
+  ## list(fun, args). args itself is a list.
+  ## So the first do.call will create the call: do.call(fun, args)
+  ## This second do.call will perform fun(args).
+  ##
+  ## The c() is needed because the first .GlobalEnv is itself
+  ## an argument to the do.call within the do.call.
+  ##
+  ## It is important that we do not create any temporary variables,
+  ## the function is called from an empty global environment.
+
+  script <- quote({
+    saveRDS(
+      do.call(
+        do.call,
+        c(readRDS("{expr_file}"), list(envir = .GlobalEnv)),
+        envir = .GlobalEnv
+      ),
+      file = "{res}"
+    )
+  })
+
+  script <- deparse(script)
+  script <- gsub("\n", ";", script, fixed = TRUE)
 
   tmp <- tempfile()
   script <- gsub("{expr_file}", expr_file, script, fixed = TRUE)
