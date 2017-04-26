5dec37f978648c1fa44c1e808da8d3814a71f170
Gábor Csárdi <csardi.gabor@gmail.com>
2017-04-26 12:38:03 +0100

    More background work, export rcmd_process

---
+++ tests/testthat/test-rcmd-bg.R
@@ -0,0 +1,26 @@
+
+context("rcmd_bg")
+
+test_that("rcmd_bg runs", {
+  x <- rcmd_bg_safe("config", "CC")
+  x$wait()
+  expect_match(x$read_output_lines(), ".")
+  expect_equal(x$get_exit_status(), 0)
+})
+
+test_that("r_cmd can be killed", {
+  cat("Sys.sleep(5)", file = tmp <- tempfile())
+  on.exit(try_silently(unlink(tmp)), add = TRUE)
+
+  x <- rcmd_bg_safe("BATCH", tmp)
+  expect_true(x$is_alive())
+
+  x$kill()
+  expect_false(x$is_alive())
+})
+
+test_that("r_cmd errors", {
+  x <- rcmd_bg_safe("config", "axaxaxaxax")
+  x$wait()
+  expect_equal(x$get_exit_status(), 1)
+})
