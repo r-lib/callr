46d4be803176c57dc16bafa57fff28b574526ce7
hadley <h.wickham@gmail.com>
2017-06-28 14:27:46 -0500

    Suppress R_ENVIRON_USER

--- R/rcmd.R
+++ R/rcmd.R
@@ -73,8 +73,35 @@ rcmd <- function(cmd, cmdargs = character(), libpath = .libPaths(),
 
 rcmd_safe <- rcmd
 
-#' \code{rcmd_safe_env} returns a set of environment variables that are
-#' more appropriate for \code{rcmd_safe}.
+#' `rcmd_safe_env` returns a set of environment variables that are
+#' more appropriate for [rcmd_safe()]. It is exported to allow manipulating
+#' these variables (e.g. add an extra one), before passing them to the
+#' [rcmd()] functions.
+#'
+#' It currently has the following variables:
+#' * `CYGWIN="nodosfilewarning"`: On Windows, do not warn about MS-DOS
+#'   style file names.
+#' * `R_TESTS=""` This variable is set by `R CMD check`, and makes the
+#'   child R process load a startup file at startup, from the current
+#'   working directory, that is assumed to be the `/test` dirctory
+#'   of the package being checked. If the current working directory is
+#'   changed to something else (as it typically is by `testthat`, then R
+#'   cannot start. Setting it to the empty string ensures that `callr` can
+#'   be used from unit tests.
+#' * `R_BROWSER="false"`: typically we don't want to start up a browser
+#'   from the child R process.
+#' * `R_PDFVIEWER="false"`: similarly for the PDF viewer.
+#' * `R_ENVIRON_USER=tempfile()`: this prevents R from loading the user
+#'   `.Renviron`.
+#' * `NOT_CRAN="true"`: if `NOT_CRAN` is not set to anything else, then
+#'   it is set to `"true"`.
+#'
+#' Note that `callr` also sets the `R_LIBS`, `R_LIBS_USER`,
+#' `R_LIBS_SITE`, `R_PROFILE` and `R_PROFILE_USER` environment variables
+#' appropriately, unless these are set by the user in the `env` argument
+#' of the `r`, etc. calls.
+#'
+#' @return A named character vector of environment variables.
 #'
 #' @export
 #' @rdname rcmd_safe
@@ -85,7 +112,8 @@ rcmd_safe_env <- function() {
     CYGWIN = "nodosfilewarning",
     R_TESTS = "",
     R_BROWSER = "false",
-    R_PDFVIEWER = "false"
+    R_PDFVIEWER = "false",
+    R_ENVIRON_USER = tempfile()
   )
 
   if (is.na(Sys.getenv("NOT_CRAN", unset = NA))) {
