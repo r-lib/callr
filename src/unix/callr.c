9c3882700d43e42604e0761f7a0f58bdf7d1d96f
Gábor Csárdi <csardi.gabor@gmail.com>
2018-03-28 19:29:27 +0100

    Unix: kill(2) might return EPERM, handle that
    Fixes #46.

--- src/unix/callr.c
+++ src/unix/callr.c
@@ -695,7 +695,7 @@ SEXP callr_kill(SEXP status, SEXP grace) {
 
   /* It is still running, so a SIGKILL */
   int ret = kill(-pid, SIGKILL);
-  if (ret == -1 && errno == ESRCH) { goto cleanup; }
+  if (ret == -1 && (errno == ESRCH || errno == EPERM)) { goto cleanup; }
   if (ret == -1) {
     callr__unblock_sigchld();
     error("process_kill: %s", strerror(errno));
