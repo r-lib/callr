d0ea2b67cf437b80f21d8a3d5674c003a07cfe28
Gábor Csárdi <csardi.gabor@gmail.com>
2018-10-04 11:21:23 +0100

    Test for r_session reading out stdout/err on error

---
+++ tests/testthat/test-r-session-messages.R
@@ -0,0 +1,36 @@
+
+context("r_session messages")
+
+test_that("callr_message, then error", {
+  rs <- r_session$new()
+  on.exit(rs$kill(), add = TRUE)
+
+  do <- function() {
+    msg <- structure(list(message = "hi"),
+                     class = c("callr_message", "condition"))
+    signalCondition(msg)
+    signalCondition(msg)
+    stop("nah-ah")
+  }
+
+  msg <- err <- NULL
+  tryCatch(
+    withCallingHandlers(
+      rs$run(do),
+      callr_message = function(m) msg <<- c(msg, list(m))),
+    error = function(e) err <<- e)
+
+  expect_s3_class(msg[[1]], "callr_message")
+  expect_equal(conditionMessage(msg[[1]]), "hi")
+  expect_s3_class(msg[[2]], "callr_message")
+  expect_equal(conditionMessage(msg[[2]]), "hi")
+
+  expect_s3_class(err, "error")
+  expect_equal(conditionMessage(err), "nah-ah")
+
+  expect_true(rs$is_alive())
+  expect_equal(rs$get_state(), "idle")
+  expect_identical(rs$read_error_lines(), character())
+
+  rs$close()
+})
