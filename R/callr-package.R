d5980d7db4566aacecd093f5ef5d1df4f52f8b5f
Gábor Csárdi <csardi.gabor@gmail.com>
2022-08-22 12:19:32 +0200

    Re-generate docs with new asciicast
    Manual has HTML output now, README has SVGs, and we also test
    that we have no leftover SVG files.
    
    This fails the HTML5 check currently, we need to update
    asciicast to pass that.

--- R/callr-package.R
+++ R/callr-package.R
@@ -1,11 +1,15 @@
 
 #' Call R from R
 #'
+#' @description
 #' It is sometimes useful to perform a computation in a separate
 #' R process, without affecting the current R process at all.  This
 #' packages does exactly that.
+#' `r paste(asciicast::print_html_style(), collapse = "\n")`
 #'
-#' ```{r child = "README.Rmd"}
+#' # callr
+#'
+#' ```{r man-readme, child = "README.Rmd"}
 #' ```
 #'
 #' @keywords internal
---
+++ tests/testthat/test-package.R
@@ -0,0 +1,13 @@
+
+test_that("all figures in man/figures are needed", {
+  skip_on_cran()
+  pkg_dir <- test_package_root()
+  figs <- dir(file.path(pkg_dir, "man", "figures"))
+  readme <- file.path(pkg_dir, "README.md")
+  readme_figs <- grep("man/figures/", readLines(readme), value = TRUE)
+  readme_figs <- sub("^.*man/figures/(.*[.]svg).*$", "\\1", readme_figs)
+  expect_equal(
+    sort(figs),
+    sort(readme_figs)
+  )
+})
