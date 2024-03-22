68ffe0347dcfe2c94e3dbac26d18046600c53952
Gábor Csárdi <csardi.gabor@gmail.com>
2024-03-22 16:01:28 +0100

    Do not run tests on CRAN

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,12 +1,13 @@
-
 library(callr)
 
-if (callr:::is_false_check_env_var("_R_CHECK_FORCE_SUGGESTS_")) {
-  if (requireNamespace("testthat", quietly = TRUE)) {
+if (Sys.getenv("NOT_CRAN") == "true") {
+  if (callr:::is_false_check_env_var("_R_CHECK_FORCE_SUGGESTS_")) {
+    if (requireNamespace("testthat", quietly = TRUE)) {
+      library(testthat)
+      test_check("callr")
+    }
+  } else {
     library(testthat)
     test_check("callr")
   }
-} else {
-  library(testthat)
-  test_check("callr")
 }
