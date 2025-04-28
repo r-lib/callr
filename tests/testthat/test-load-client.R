ff890806185edc96067793d0276f7e7cd77222a7
Gábor Csárdi <csardi.gabor@gmail.com>
2025-04-28 10:49:00 +0200

    Embed some mocking helpers instead of mockery

--- tests/testthat/test-load-client.R
+++ tests/testthat/test-load-client.R
@@ -13,20 +13,18 @@ test_that("load_client_lib", {
 })
 
 test_that("errors", {
-  skip_if_not_installed("mockery")
-  mockery::stub(load_client_lib, "system.file", "")
+  fake(load_client_lib, "system.file", "")
   expect_snapshot(error = TRUE, load_client_lib())
 })
 
 test_that("errors 2", {
-  skip_if_not_installed("mockery")
   sofile <- system.file(
     "libs",
     .Platform$r_arch,
     paste0("client", .Platform$dynlib.ext),
     package = "processx"
   )
-  mockery::stub(load_client_lib, "dyn.load", function(...) stop("ooops"))
+  fake(load_client_lib, "dyn.load", function(...) stop("ooops"))
   expect_snapshot(error = TRUE, load_client_lib(sofile))
 })
 
